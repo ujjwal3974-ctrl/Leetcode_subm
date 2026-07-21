b740d147f2cc3106837dfed34cfd1761f491c367
ujjwal3974-ctrl <ujjwal3974@gmail.com>
2026-07-22 00:37:57 +0530

    Time: 0 ms (100.00%) | Memory: 30 MB (27.35%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -2,12 +2,20 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> temp(n);
         
-        for(int i = 0; i < n; i++){
-            temp[(k+i)%n] =nums[i];
-            
+        //copy elementx b4 k in temp and copy it later in empty spaces
+        if (n == 0) return;
+        k = k % n;
+        vector<int> temp(k);
+        for (int i = n - k; i < n; i++) {
+            temp[i - n + k] = nums[i];
         }
-        nums = temp;
+        for (int i = n - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+
     }
 };
