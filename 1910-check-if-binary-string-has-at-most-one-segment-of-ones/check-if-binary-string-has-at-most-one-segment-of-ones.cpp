3ba0d9ccaa5a8ddbfba1b9d9d8749a5db9c7a9b4
ujjwal3974-ctrl <ujjwal3974@gmail.com>
2026-08-03 00:35:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (46.05%) - LeetSync

---
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        int count = 0;
+        for(int i = 1; i<s.length(); i++){
+            if(s[i-1] == '0' && s[i] == '1'){
+                return false;
+            }            
+        }
+        return true;
+    }
+};
